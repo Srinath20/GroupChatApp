a68eb66624f3516fbd106b592cdf871d18c72555
Srinath20 <srinathgollapinni>
2024-08-30 09:17:00 +0530

    Signup page

---
+++ signup.js
@@ -0,0 +1,8 @@
+document.getElementById("signupForm").addEventListener("submit", function (event) {
+    // Example: You can add custom JavaScript validation or form handling here.
+    // Prevent form submission for demonstration purposes.
+    // event.preventDefault();
+
+    // You can also handle form submission with AJAX here if needed.
+    console.log("Form submitted!");
+});
